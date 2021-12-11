b01f28885fecc46b3bf056235e2569a76c0c7215
Odiri Teddie <odiriteddie.adamu@gmail.com>
2021-12-11 03:41:59 -0500

    more fixes

--- index.js
+++ index.js
@@ -3,16 +3,41 @@ const accordionItemHeaders = document.querySelectorAll('.accordion-item-header')
 const nav = document.querySelector('nav');
 const toggleBtn = document.querySelector('.mobile-nav-toggle');
 
-const tabContainer = document.querySelector('.button-container');
+//////////////////// Gallery Section
+
+const tabContainer = document.querySelector('.tab-container');
 
 const pictureTab = document.querySelector('.gallery__picture');
+
 const videoTab = document.querySelector('.gallery__video');
 
-const galleryPictures = document.querySelector('.gallery-pictures_content')
+const galleryPictures = document.querySelector('.gallery-pictures_content');
 
-const galleryVideos = document.querySelector('.gallery-videos_content')
+const galleryVideos = document.querySelector('.gallery-videos_content');
+
+///////////////////// User Account Section
+
+const userProfileSection = document.querySelector('.user-detail__list');
+
+const coursePurchased = document.querySelector('.user-details__course');
+
+const profileDetails = document.querySelector('.user-details__profile');
+
+const proLogin = document.querySelector('.user-details__pro-login');
+
+const userHelp = document.querySelector('.user-details__help');
 
 
+const purchased = document.querySelector('.user-details__content_course')
+
+const details = document.querySelector('.user-details__content_profile')
+
+const loginss = document.querySelector('.user-details__content_pro-login')
+
+const helppp = document.querySelector('.user-details__content_help')
+
+
+////////////////////////// FAQ Section
 
 accordionItemHeaders.forEach(accordionItemHeader => {
     accordionItemHeader.addEventListener('click', event => {
@@ -44,27 +69,88 @@ accordionItemHeaders.forEach(accordionItemHeader => {
     });
 });
 
+///////////////////////// Menu Section
 
-function toggleNav(e) {
+function toggleNav() {
     nav.classList.toggle('show');  
 }
 
 
+
+///////////////////////// Gallery Section
+
+
 function toggleTab(e) {
+
    if(e.target == videoTab ) {
-       pictureTab.classList.remove('current-gallery');
        videoTab.classList.add('current-gallery');
-       galleryPictures.classList.remove('gallery-active');
+       pictureTab.classList.remove('current-gallery');
        galleryVideos.classList.add('gallery-active');
+       galleryPictures.classList.remove('gallery-active');
+       console.log('yes');
+
    } else if(e.target == pictureTab ) {
-       videoTab.classList.remove('current-gallery');
+
        pictureTab.classList.add('current-gallery');
-       galleryVideos.classList.remove('gallery-active');
+       videoTab.classList.remove('current-gallery');
        galleryPictures.classList.add('gallery-active');
+       galleryVideos.classList.remove('gallery-active');
+       onsole.log('false');
    };
-
-
 }
+
+
+
+////////////////////// User Account Section
+
+
+function toggleUserSection(e) {
+    if(e.target == coursePurchased ) {
+        coursePurchased.classList.add('current');
+        profileDetails.classList.remove('current');
+        proLogin.classList.remove('current');
+        userHelp.classList.remove('current');
+
+        purchased.classList.add('user-active')
+        details.classList.remove('user-active');
+        loginss.classList.remove('user-active');
+        helppp.classList.remove('user-active');
+
+    } else if(e.target == profileDetails) {
+        profileDetails.classList.add('current');
+        coursePurchased.classList.remove('current');
+        proLogin.classList.remove('current');
+        userHelp.classList.remove('current');
+
+        details.classList.add('user-active');
+        purchased.classList.remove('user-active')
+        loginss.classList.remove('user-active');
+        helppp.classList.remove('user-active');
+
+    } else if(e.target == proLogin) {
+        proLogin.classList.add('current');
+        profileDetails.classList.remove('current');
+        coursePurchased.classList.remove('current');
+        userHelp.classList.remove('current');
+
+        loginss.classList.add('user-active');
+        details.classList.remove('user-active');
+        purchased.classList.remove('user-active')
+        helppp.classList.remove('user-active');
+
+    }  else if(e.target == userHelp) {
+        userHelp.classList.add('current');
+        proLogin.classList.remove('current');
+        profileDetails.classList.remove('current');
+        coursePurchased.classList.remove('current');
+
+        helppp.classList.add('user-active');
+        loginss.classList.remove('user-active');
+        details.classList.remove('user-active');
+        purchased.classList.remove('user-active')
+    } 
+}
+
 
 
 
@@ -72,5 +158,7 @@ function toggleTab(e) {
 
 toggleBtn.addEventListener('click', toggleNav);
 
-tabContainer.addEventListener('click', toggleTab);
+userProfileSection.addEventListener('click', toggleUserSection);
 
+console.log('yes');
+tabContainer.addEventListener('click', toggleTab);
