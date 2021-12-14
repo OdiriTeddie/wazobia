af518c59d85e46d7266c53b3e063257465525a96
Odiri Teddie <odiriteddie.adamu@gmail.com>
2021-12-14 07:47:46 -0500

    more fixes

--- index.js
+++ index.js
@@ -37,6 +37,80 @@ const loginss = document.querySelector('.user-details__content_pro-login')
 const helppp = document.querySelector('.user-details__content_help')
 
 
+///////////////////// Gallery Modal Light Box
+const gallery = document.querySelectorAll('.gallery-pictures_content .image')
+const previewBox = document.querySelector('.preview-box');
+const previewImg = previewBox.querySelector('img')
+const closeIcon = previewBox.querySelector('.icon');
+const currentImg = previewBox.querySelector('.current-img');
+const totalImg = previewBox.querySelector('.total-img');
+const shadow = document.querySelector('.shadow');
+
+window.onload = () => {
+    for(let i = 0; i < gallery.length; i++) {
+        totalImg.textContent = gallery.length; // passing gallery images length to totalImg
+        let newIndex = i; // padding i value to newIndex variable
+        let clickImgIndex;
+        gallery[i].onclick = ()=> {
+            clickImgIndex = newIndex; //padding clicked img index to clickimgIndex variable
+            console.log(i);
+            
+            function preview() {
+                currentImg.textContent = newIndex + 1; //passing newIndex value to currentImg by adding +1
+                let selectedImgUrl = gallery[newIndex].querySelector('img').src; //getting user clicked image url
+                previewImg.src = selectedImgUrl // passing user clicked img url to previewImg source
+
+            }
+
+            // previous and Next button
+            const prevBtn = document.querySelector('.prev');
+            const nextBtn = document.querySelector('.next');
+            if(newIndex == 0) {
+                prevBtn.style.display = 'none';
+            }
+            if(newIndex >= gallery.length - 1) {
+                nextBtn.style.display = 'none';
+            }
+            prevBtn.onclick = () => {
+                newIndex--; // decrement newIndexvalue
+                if(newIndex == 0) {
+                    preview();
+                    prevBtn.style.display = 'none';
+                } else {
+                    preview(); // calling again above function to image
+                    nextBtn.style.display = 'block'
+                }
+            }
+
+            nextBtn.onclick = () => {
+                newIndex++; // decrement newIndexvalue
+                if(newIndex >= gallery.length - 1) {
+                    preview();
+                    prevBtn.style.display = 'none';
+                } else {
+                    preview(); // calling again above function to image
+                    nextBtn.style.display = 'block'
+                }
+            }
+
+
+            preview(); //calling above function
+            previewBox.classList.add('show');
+            shadow.style.display = 'block';
+            document.querySelector('body').style.overflow = 'hidden';
+
+            closeIcon.onclick = () => {
+                newIndex  = clickImgIndex; //assigning users first click img index to newIndex varialbe
+                prevBtn.style.display = 'block'
+                nextBtn.style.display = 'block'
+                previewBox.classList.remove('show');
+                shadow.style.display = 'none';
+                document.querySelector('body').style.overflow = 'auto';
+            }
+        }
+    }
+}
+
 ////////////////////////// FAQ Section
 
 accordionItemHeaders.forEach(accordionItemHeader => {
